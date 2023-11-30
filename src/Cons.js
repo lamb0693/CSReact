ac30b724ec74a2d99e2e3645cc7d07b1c89a7088
lamb0693 <mar189@naver.com>
2023-11-30 09:29:37 +0900

    modify server add

--- src/Cons.js
+++ src/Cons.js
@@ -1,4 +1,5 @@
-export const SERVER_ADDRESS = "http://localhost:8080"
+//export const SERVER_ADDRESS = "http://localhost:8080"
 //export const SERVER_ADDRESS = "http://15.164.61.120:8080"
 
 //export const SERVER_ADDRESS = "http://192.168.200.198:8080"
+export const SERVER_ADDRESS = "http://10.100.203.62:8080"
